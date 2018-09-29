e186b75980f8b2022a2e824c534cf294d6a51ad7
tongfor <tyuziyu@163.com>
2018-09-29 18:01:39 +0800

    顶部菜单JS化

---
+++ YhcdWebsite/wwwroot/js/json/topMenu.js
@@ -0,0 +1,34 @@
+﻿//页面顶部菜单项目
+var topMenu = [{
+    "id": 1,
+    "menuTitle": "首页",
+    "menuLink": "/"
+}, {
+    "id": 2,
+    "menuTitle": "公司简介",
+    "menuLink": "/Home/About.html"
+}, {
+    "id": 3,
+    "menuTitle": "专利服务",
+    "menuLink": "/Home/Patent.html"
+}, {
+    "id": 4,
+    "menuTitle": "商标版权",
+    "menuLink": "/Home/Trademark.html"
+}, {
+    "id": 5,
+    "menuTitle": "资质认证",
+    "menuLink": "/Home/Qualification.html"
+}, {
+    "id": 6,
+    "menuTitle": "科技咨询",
+    "menuLink": "/Home/Consulting.html"
+}, {
+    "id": 7,
+    "menuTitle": "行业资讯",
+    "menuLink": "/Article/List.html?id=20"
+}, {
+    "id": 8,
+    "menuTitle": "联系我们",
+    "menuLink": "/#contact"
+}];
---
+++ YhcdWebsite/wwwroot/js/topMenu.js
@@ -0,0 +1,35 @@
+﻿; var topMenuFunc = (function () {
+    var menuObjs = $("#top>div>ul")
+    var currentUrl = window.location.href
+
+    //菜单初始化
+    var _topMenuInit = function () {
+        menuObjs.empty()
+        for (var p in topMenu) {
+            var li = '<li><a href="' + topMenu[p].menuLink + '">' + topMenu[p].menuTitle + '</a></li>'
+            menuObjs.append(li)
+        }
+    }
+
+    //菜单匹配当前菜单
+    var _topMenuMatch = function () {
+        menuObjs.children("li").each(function (i, e) {
+            var href = $(this).find("a").attr("href")
+            if (currentUrl.split('?')[0].indexOf(href.split('?')[0]) !== -1 && href !== "/") {
+                $(this).addClass("current")
+            }
+            else {
+                $(this).removeClass("current")
+            }
+        })
+    }
+    return {
+        topMenuInit: _topMenuInit,
+        topMenuMatch: _topMenuMatch
+    }
+}())
+
+$(function () {
+    topMenuFunc.topMenuInit()
+    topMenuFunc.topMenuMatch()
+});
