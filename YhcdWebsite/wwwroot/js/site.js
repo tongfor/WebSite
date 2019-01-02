fb4fa2395ca16a78b5a581d9e56f8d356d90647f
tongfor <tyuziyu@163.com>
2019-01-02 17:03:34 +0800

    修复 IE下不支持CSS动画

--- YhcdWebsite/wwwroot/js/site.js
+++ YhcdWebsite/wwwroot/js/site.js
@@ -22,98 +22,54 @@ Date.prototype.Format = function (fmt) { //author: meizz
     return fmt;
 };
 
-(function () {
-    var slideMenu = function () {
-        //竖风琴特效
-        var sp, st, t, m, sa, l, w, gw, ot;
-        return {
-            destruct: function () {
-                if (m) {
-                    clearInterval(m.htimer);
-                    clearInterval(m.timer);
-                }
-            },
-            build: function (sm, sw, mt, s, sl, h) {
-                sp = s;
-                st = sw;
-                t = mt;
-                m = document.getElementById(sm);
-                sa = m.getElementsByTagName('li');
-                l = sa.length;
-                w = m.offsetWidth;
-                gw = w / l;
-                ot = Math.floor((w - st) / (l - 1));
-                var i = 0;
-                for (i; i < l; i++) {
-                    s = sa[i];
-                    s.style.width = gw + 'px';
-                    this.timer(s)
-                }
-                if (sl != null) {
-                    m.timer = setInterval(function () {
-                        slideMenu.slide(sa[sl - 1])
-                    }, t)
-                }
-            },
-            timer: function (s) {
-                s.onmouseover = function () {
-                    clearInterval(m.htimer);
-                    clearInterval(m.timer);
-                    m.timer = setInterval(function () {
-                        slideMenu.slide(s)
-                    }, t);
-                    //console.log($(this).find('.mask_b').html());
-                    $(this).find('.mask_b').hide();
-                    //console.log($(this).find('.mask_b').attr("style"));
-                }
-                s.onmouseout = function () {
-                    clearInterval(m.timer);
-                    clearInterval(m.htimer);
-                    m.htimer = setInterval(function () {
-                        slideMenu.slide(s, true)
-                    }, t);
-                    //console.log($(this).find('.mask_b').html());
-                    $(this).find('.mask_b').show();
-                    //console.log($(this).find('.mask_b').attr("style"));
-                }
-            },
-            slide: function (s, c) {
-                var cw = parseInt(s.style.width);
-                if ((cw < st && !c) || (cw > gw && c)) {
-                    var owt = 0; var i = 0;
-                    for (i; i < l; i++) {
-                        if (sa[i] != s) {
-                            var o, ow; var oi = 0; o = sa[i]; ow = parseInt(o.style.width);
-                            if (ow < gw && c) {
-                                oi = Math.floor((gw - ow) / sp);
-                                oi = (oi > 0) ? oi : 1;
-                                o.style.width = (ow + oi) + 'px';
-                                //console.log(o);
-                                //console.log(o.style.width);
-                            } else if (ow > ot && !c) {
-                                oi = Math.floor((ow - ot) / sp);
-                                oi = (oi > 0) ? oi : 1;
-                                o.style.width = (ow - oi) + 'px';
-                                //console.log(o);
-                                //console.log(o.style.width);
-                            }
-                            if (c) {
-                                owt = owt + (ow + oi)
-                            } else {
-                                owt = owt + (ow - oi)
-                            }
-                        }
-                    }
-                    s.style.width = (w - owt) + 'px';
-                } else {
-                    if (m.htimer)
-                        clearInterval(m.htimer)
-                    if (m.timer)
-                        clearInterval(m.timer);
-                }
-            }
-        };
-    }();
-    slideMenu.build('sm', 280, 20, 10, 1);
-})();
+$(document).ready(function () {
+    if (window.ActiveXObject || "ActiveXObject" in window) {
+        //ie brower
+        $(".logo").hide();
+        $(".logo-small").removeClass("hidden-lg").removeClass("hidden-md");
+    }
 
+    $("#top .navBtn").click(function () {
+        if ($("#top ul:visible").length == 0) {
+            $("#top ul").show()
+        } else {
+            $("#top ul").hide()
+        }
+
+        $("#top .userBtn").click(function () {
+            if ($("#top .searck:visible").length == 0) {
+                $("#top .searck").show()
+            } else {
+                $("#top .searck").hide()
+            }
+        })
+    })
+
+    $("#banner").slide({ mainCell: ".bd ul", autoPlay: true });
+
+
+    $("#leftsead a").hover(function () {
+        if ($(this).prop("className") == "youhui") {
+            $(this).children("img.hides").show();
+        } else {
+            $(this).children("img.hides").show();
+            $(this).children("img.shows").hide();
+            $(this).children("img.hides").animate({ marginRight: '0px' }, 'slow');
+        }
+    }, function () {
+        if ($(this).prop("className") == "youhui") {
+            $(this).children("img.hides").hide('slow');
+        } else {
+            $(this).children("img.hides").animate({ marginRight: '-143px' }, 'slow', function () { $(this).hide(); $(this).next("img.shows").show(); });
+        }
+    });
+
+    $("#top_btn").click(function () { if (scroll == "off") return; $("html,body").animate({ scrollTop: 0 }, 600); });
+
+    setTimeout(animation_gradient_run, 1500)
+});
+
+function animation_gradient_run() {
+    $(".logo").removeClass("logo-animation-revolveDrop");
+    $(".logo").addClass("logo-animation-gradient");
+}
